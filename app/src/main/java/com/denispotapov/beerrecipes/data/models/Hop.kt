03550053029d25c2a763901f98f1c958305e8562
denispotapov <fantom.r45@mail.ru>
2023-11-01 20:37:45 +0300

    add companion object

--- app/src/main/java/com/denispotapov/beerrecipes/data/models/Hop.kt
+++ app/src/main/java/com/denispotapov/beerrecipes/data/models/Hop.kt
@@ -1,3 +1,11 @@
 package com.denispotapov.beerrecipes.data.models
 
-data class Hop(val name: String, val alphaAcid: String, val boilingTime: String, val weight: Int)
+data class Hop(val name: String, val alphaAcid: String, val boilingTime: String, val weight: Int) {
+
+    companion object {
+
+
+
+    }
+
+}
