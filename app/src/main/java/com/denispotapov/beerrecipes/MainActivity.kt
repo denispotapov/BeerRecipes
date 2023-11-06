5f306a316d12f54d4abfd14e0cdcd64471d9f8c5
denispotapov <fantom.r45@mail.ru>
2023-11-06 22:53:13 +0300

    added text if list of recipes empty / added floating action button for create recipe

--- app/src/main/java/com/denispotapov/beerrecipes/MainActivity.kt
+++ app/src/main/java/com/denispotapov/beerrecipes/MainActivity.kt
@@ -3,14 +3,30 @@ package com.denispotapov.beerrecipes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.denispotapov.beerrecipes.data.models.Recipe
 import com.denispotapov.beerrecipes.ui.theme.BeerRecipesTheme
+import com.denispotapov.beerrecipes.ui.theme.Pink40
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +36,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Pink40
                 ) {
-                    Greeting("Android")
+                    TestView()
                 }
             }
         }
@@ -30,17 +46,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TestView() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
+
+        val listRecipe = emptyList<Recipe>()
+
+        if (listRecipe.isNotEmpty()) {
+            val listState = rememberLazyListState()
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                state = listState
+            ) {
+
+            }
+        } else {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(Color.Red)
+                    .align(alignment = Alignment.Center),
+                textAlign = TextAlign.Center,
+                text = "Вы еще не создали ни одного рецепта"
+            )
+        }
+
+        FloatingActionButton(
+            modifier = Modifier
+                .padding(20.dp)
+                .align(Alignment.BottomEnd),
+            onClick = {},
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Добавить")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BeerRecipesTheme {
-        Greeting("Android")
+        TestView()
     }
 }
